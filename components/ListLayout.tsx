e96534439d17480023c65f447b8cbe980e7b7d40
ryuheeyoung <rhy901008@gmail.com>
2022-08-23 21:41:56 +0900

    [리팩토링] 레이아웃 수정
    - ListLayout.tsx: 레이아웃 템플리 변경

--- components/ListLayout.tsx
+++ components/ListLayout.tsx
@@ -2,13 +2,24 @@ import Head from "next/head";
 import styled from "styled-components";
 import { LayoutProps } from "./Layout";
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  height: calc(100vh - 16px);
+  align-items: center;
+`;
 const Title = styled.h1`
   border-bottom: 1px solid ${(props) => props.theme.colors.borderColor};
   color: ${(props) => props.theme.colors.primary};
-  height: 55px;
-  display: flex;
-  align-items: center;
   text-shadow: 0 0 2px ${(props) => props.theme.colors.white};
+  text-align: center;
+  flex: 1 1 0;
+`;
+const Content = styled.div`
+  flex: 15 0 90vh;
+  overflow: hidden;
+}
 `;
 
 const ListLayout = ({ children }: LayoutProps) => {
@@ -17,8 +28,11 @@ const ListLayout = ({ children }: LayoutProps) => {
       <Head>
         <title>UserList</title>
       </Head>
-      <Title>사용자 리스트</Title>
-      <main>{children}</main>
+
+      <Main>
+        <Title>사용자 리스트</Title>
+        <Content>{children}</Content>
+      </Main>
     </>
   );
 };
