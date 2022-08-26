2249a1f1964e666f5410d7dc17119fb367c806da
ryuheeyoung <rhy901008@gmail.com>
2022-08-26 17:50:41 +0900

    [리팩토링] 확장자 변경

---
+++ utils/users.tsx
@@ -0,0 +1,42 @@
+import { IUser } from "../interfaces/user";
+import { v4 as uuidv4 } from "uuid";
+
+export interface Result {
+  page: number;
+  size: number;
+  result: IUser[];
+}
+
+/**
+ *
+ * @param page
+ * @param size
+ * @returns {Result}
+ */
+export const makeDatas = (page: number, size: number): Result => {
+  const start = (page - 1) * size + 1;
+  const users: IUser[] = [];
+
+  for (let i = start; i <= page * size; i++) {
+    for (let j = 0; j <= 500; j++) {
+      console.log("It's a very slow API...1..." + j);
+    }
+
+    const id = i;
+    const key = Math.random().toString(36).slice(-8);
+    const uuid: string = uuidv4(key);
+
+    users.push({
+      id,
+      key,
+      uuid,
+      created_at: Date.now(),
+    });
+  }
+
+  return {
+    page,
+    size,
+    result: users,
+  };
+};
