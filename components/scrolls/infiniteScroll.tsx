39276ea54681e13ea3851aad7fec8fdc273d9787
ryuheeyoung <rhy901008@gmail.com>
2022-08-26 14:44:27 +0900

    [리팩토링] 무한스크롤 기본 로딩바 추가

--- components/scrolls/infiniteScroll.tsx
+++ components/scrolls/infiniteScroll.tsx
@@ -1,11 +1,40 @@
 import { useCallback, useEffect, useRef } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const loading_kf = keyframes`
+  0% {
+      max-width: 0;
+  }
+  100% {
+      max-width: 100%;
+  }
+`;
 
 const Scroll = styled.div`
   display: block;
   width: 100%;
   height: fit-content;
   min-height: 10px;
+`;
+
+const Loader = styled.div`
+  position: relative;
+  grid-column-end: 4;
+  padding: 0.5em;
+  font-size: 1.2em;
+  color: ${({ theme }) => theme.colors.white};
+  text-shadow: 1px 1px 2px ${({ theme }) => theme.colors.point};
+  text-align: center;
+  &:before {
+    content: attr(data-text);
+    text-align: center;
+    position: absolute;
+    overflow: hidden;
+    max-width: 100%;
+    white-space: nowrap;
+    color: ${({ theme }) => theme.colors.dark};
+    animation: ${loading_kf} 5s infinite 1s;
+  }
 `;
 
 export interface ScrollProps {
@@ -41,5 +70,13 @@ export const InfiniteScroll = ({
     return () => observer.disconnect();
   }, [handleIntersect, threshold]);
 
-  return <Scroll ref={ref}>{children}</Scroll>;
+  return (
+    <Scroll ref={ref}>
+      {children ? (
+        children
+      ) : (
+        <Loader data-text="now loading ...">now loading ...</Loader>
+      )}
+    </Scroll>
+  );
 };
