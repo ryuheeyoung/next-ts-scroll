9b84df4ab16a165090b474398f67e2d8fd8e6421
ryuheeyoung <rhy901008@gmail.com>
2022-08-26 17:56:14 +0900

    [리팩토링] 스타일 분리 (pages 스타일 분리)
    - index.tsx
    - sharedStyles.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,8 @@
-import styled from "styled-components";
+import { Layouts } from "components/styles/sharedStyles";
+
 import ListForm from "components/ListForm/ListForm";
 import ListLayout from "components/ListLayout";
-
-const Layouts = styled.div`
-  display: flex;
-  position: relative;
-  flex-flow: row wrap;
-  height: 100%;
-  padding: 1.25em;
-  flex: auto;
-  gap: 1.5em;
-  & > div {
-    height: 100%;
-    margin: 0;
-    overflow: auto;
-    border-radius: 5px;
-    padding: 0.25em;
-    color: ${({ theme }) => theme.colors.dark};
-    &:nth-child(1) {
-      flex: 3 0 200px;
-      box-shadow: 0 0 5px ${({ theme }) => theme.colors.highlight};
-      background: ${({ theme }) => theme.colors.primary};
-    }
-    &:nth-child(2) {
-      flex: 2 0 0;
-      box-shadow: 0 0 5px ${({ theme }) => theme.colors.secondary};
-      background: ${({ theme }) => theme.colors.secondary};
-    }
-    &:nth-child(3) {
-      flex: 2 0 0;
-      box-shadow: 0 0 5px ${({ theme }) => theme.colors.point};
-      background: ${({ theme }) => theme.colors.point};
-    }
-  }
-`;
+import ColorForm from "components/ColorForm/ColorForm";
 
 const IndexPage = ({ ...props }) => {
   return (
@@ -41,7 +10,9 @@ const IndexPage = ({ ...props }) => {
       <div>
         <ListForm result={props.result} />
       </div>
-      <div>hello</div>
+      <div>
+        <ColorForm />
+      </div>
       <div> hi</div>
     </Layouts>
   );
